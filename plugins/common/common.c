1903b3bd987ce915fafed32f3c96ad515a56e9d5
Sam van Kampen <sam@tehsvk.net>
2014-11-16 18:21:03 +0100

    Add plugin listing command

--- plugins/common/common.c
+++ plugins/common/common.c
@@ -54,6 +54,31 @@ void part_cmd(struct bot *bot, char *nick, char *chan, char *args) {
 	sockprintf(bot->socket, "PART :%s", chan);
 }
 
+void plugs_cmd(struct bot *bot, char *nick, char *chan, char *args) {
+	char **keys = hashmap_keys(bot->plugins);
+	int key_str_len = 0;
+	for (int i = 0; i < bot->plugins->length; ++i) {
+		key_str_len += strlen(keys[i]);
+		key_str_len += 2;
+	}
+	key_str_len--;
+
+	char *key_str = calloc(key_str_len, 1);	
+	int key_str_idx = 0;
+	
+	for (int i = 0; i < bot->plugins->length; ++i) {
+		if (i == (bot->plugins->length - 1)) {
+			key_str_idx += sprintf(key_str + key_str_idx, "%s", keys[i]);
+		} else {
+			key_str_idx += sprintf(key_str+key_str_idx, "%s, ", keys[i]);
+		}
+	}
+
+	bot->proto->msg(chan, "Loaded plugins: %s", key_str);
+	free(keys);
+	free(key_str);
+}
+
 void bash_cmd(struct bot *bot, char *nick, char *chan, char *args) {
 	if (!args) {
 		bot->proto->msg(chan, "usage: sh <command>");
@@ -136,6 +161,7 @@ void init(struct bot *bot) {
 	hashmap_set("part", part_cmd, bot->commands);
 	hashmap_set("sh", bash_cmd, bot->commands);
 	
+	hashmap_set("plugins", plugs_cmd, bot->commands);
 	hashmap_set("plugin.load", plugin_load_cmd, bot->commands);
 	hashmap_set("plugin.unload", plugin_unload_cmd, bot->commands);
 }
@@ -146,6 +172,7 @@ void destroy(struct bot *bot) {
 	hashmap_remove("part", bot->commands);
 	hashmap_remove("sh", bot->commands);
 	
+	hashmap_remove("plugins", bot->commands);
 	hashmap_remove("plugin.load", bot->commands);
 	hashmap_remove("plugin.unload", bot->commands);
 
