e852cbe0280602fe861a4889c6075c5cdac75c22
Sam van Kampen <sam@tehsvk.net>
2015-01-02 23:21:25 +0100

    Add trigraph support in createplug.py

--- plugins/createplug.py
+++ plugins/createplug.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-c = os.system("gcc -fPIC -shared -I../include -I../hashmap/include -L../bin -lhashmap -o ../bin/plugins/plug.so -ggdb -g3 .plug.c 2>&1")
+c = os.system("gcc -fPIC -shared -trigraphs -I../include -I../hashmap/include -L../bin -lhashmap -o ../bin/plugins/plug.so -ggdb -g3 .plug.c 2>&1")
 
 if (c != 0):
     sys.exit(1)
