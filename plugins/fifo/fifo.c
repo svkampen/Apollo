5e3c6d37a645f6d6c08e0f4dc18a4a6ba7fa9b7c
Sam van Kampen <sam@tehsvk.net>
2015-04-06 21:31:39 +0200

    FIFO writing function and channel support
    Move the FIFO writing code outside of fifo_tick and write the currently
    joined channels to the status FIFO.

--- plugins/fifo/fifo.c
+++ plugins/fifo/fifo.c
@@ -13,6 +13,20 @@
 
 int status_fifo = 0;
 
+void write_status(struct bot *bot) {
+	dprintf(status_fifo, "running %s on %s %s\n", bot->nick, bot->host, bot->port);
+
+	if (strcmp(bot->proto->name, "irc") == 0) {
+		dprintf(status_fifo, "chans ");
+	}
+
+	for (int i = 0; i < bot->channels->index; ++i) {
+		dprintf(status_fifo, "%s ", bot->channels->list[i]);
+	}
+
+	dprintf(status_fifo, "\n");
+}
+
 static int isfifo(char *filename) {
 	struct stat st;
 	stat(filename, &st);
@@ -22,7 +36,7 @@ static int isfifo(char *filename) {
 void fifo_tick(struct bot *bot) {
 	status_fifo = open("status", O_WRONLY | O_NDELAY);
 	if (status_fifo != -1) {
-		dprintf(status_fifo, "Status: running, obviously. How else would you be able to read this FIFO ;)\n");
+		write_status(bot);
 		close(status_fifo);
 	}
 	status_fifo = 0;
